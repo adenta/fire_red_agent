761e921ba74115a615683b6fb3eba0431f387c79
Andrew Denta <andrew.denta@gmail.com>
2024-12-06 18:25:05 -0500

    add memory reader

--- Gemfile
+++ Gemfile
@@ -34,20 +34,28 @@ gem "thruster", require: false
 # Use Active Storage variants [https://guides.rubyonrails.org/active_storage_overview.html#transforming-images]
 # gem "image_processing", "~> 1.2"
 
+
+
+# Async things
+gem "async"
+
+# AI
+gem "ruby-openai"
+
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
   gem "debug", platforms: %i[ mri windows ], require: "debug/prelude"
+  gem "awesome_print"
 
   # Static analysis for security vulnerabilities [https://brakemanscanner.org/]
   gem "brakeman", require: false
-
-  # Omakase Ruby styling [https://github.com/rails/rubocop-rails-omakase/]
-  gem "rubocop-rails-omakase", require: false
 end
 
 group :development do
   # Use console on exceptions pages [https://github.com/rails/web-console]
   gem "web-console"
+  gem 'rubocop', require: false
+
 end
 
 group :test do
---
+++ app/services/retroarch/memory_reader.rb
@@ -0,0 +1,55 @@
+require 'async'
+require 'async/barrier'
+require 'socket'
+
+module Retroarch
+  class MemoryReader
+    def read_bytes(address, length)
+      memory_data = Array.new(length)
+      batch_size = 50
+      Async do |task|
+        barrier = Async::Barrier.new
+
+        (0...length).step(batch_size) do |i|
+          barrier.async do
+            batch_address = address + i
+            batch_length = [batch_size, length - i].min
+            batch_data = read_bytes_packet(batch_address, batch_length)
+
+            batch_data.each_with_index do |byte, j|
+              memory_data[i + j] = byte
+            end
+          end
+        end
+
+        barrier.wait
+      end
+
+      memory_data
+    end
+
+    def read_bytes_packet(address, length)
+      message = "READ_CORE_MEMORY #{address.to_s(16)} #{length}"
+      udp_socket = UDPSocket.new
+
+      # Bind to a local port to receive the response
+      udp_socket.bind('0.0.0.0', 0)
+
+      # Send the message
+      udp_socket.send(message, 0, '127.0.0.1', 55_355)
+
+      # Set a timeout for the response (optional)
+      begin
+        udp_socket.setsockopt(Socket::SOL_SOCKET, Socket::SO_RCVTIMEO, [1, 0].pack('l_2'))
+
+        # Receive the response
+        response, _addr = udp_socket.recvfrom(1024) # 1024 is the max buffer size
+        return response.gsub("READ_CORE_MEMORY #{address.to_s(16)} ", '').strip.split(' ')
+      rescue Errno::EAGAIN
+        puts 'No response received (timeout)'
+      end
+
+      udp_socket.close
+    end
+  end
+end
