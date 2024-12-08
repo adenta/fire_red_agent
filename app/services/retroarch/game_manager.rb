162e363ef17f1f88e50be36f6594016c4666b7a0
Andrew Denta <andrew.denta@gmail.com>
2024-12-07 19:05:31 -0500

    add games to config

---
+++ app/services/retroarch/game_manager.rb
@@ -0,0 +1,62 @@
+require 'async'
+require 'async/barrier'
+require 'socket'
+
+module Retroarch
+  class MemoryReader
+    BATCH_SIZE = 50
+
+    def self.read_bytes(address, length)
+      memory_data = Array.new(length)
+      Async do |task|
+        barrier = Async::Barrier.new
+
+        (0...length).step(BATCH_SIZE) do |i|
+          barrier.async do
+            batch_address = address + i
+            batch_length = [BATCH_SIZE, length - i].min
+            batch_data = read_bytes_packet(batch_address, batch_length)
+
+            batch_data.each_with_index do |byte, j|
+              memory_data[i + j] = byte
+            end
+          end
+        end
+
+        barrier.wait
+      end
+
+      memory_data
+    end
+
+    def self.read_binary_bytes(address, length)
+      memory_data = read_bytes(address, length)
+      memory_data.map { |byte| byte.to_i(16).chr }.join
+    end
+
+    def self.read_bytes_packet(address, length)
+      message = "READ_CORE_MEMORY #{address.to_s(16)} #{length}"
+      udp_socket = UDPSocket.new
+
+      # Bind to a local port to receive the response
+      udp_socket.bind('0.0.0.0', 0)
+
+      # Send the message
+      udp_socket.send(message, 0, '127.0.0.1', 55_355)
+
+      # Set a timeout for the response (optional)
+      begin
+        udp_socket.setsockopt(Socket::SOL_SOCKET, Socket::SO_RCVTIMEO, [1, 0].pack('l_2'))
+
+        # Receive the response
+        response, _addr = udp_socket.recvfrom(1024) # 1024 is the max buffer size
+
+        return response.gsub("READ_CORE_MEMORY #{address.to_s(16)} ", '').strip.split(' ')
+      rescue Errno::EAGAIN
+        puts 'No response received (timeout)'
+      end
+
+      udp_socket.close
+    end
+  end
+end
--- app/services/retroarch/save_and_game_controller.rb
+++ app/services/retroarch/save_and_game_controller.rb
@@ -1,4 +1,12 @@
 module Retroarch
   class SaveAndGameController
+    def launch_emulator
+      Async do |task|
+        task.async do
+          command = '/Applications/RetroArch.app/Contents/MacOS/RetroArch'
+          system(command)
+        end
+      end
+    end
   end
 end
