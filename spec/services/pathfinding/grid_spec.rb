53f54ecd01bec6ceb460b6c4c3332d5224b29c57
Andrew Denta <andrew.denta@gmail.com>
2024-12-08 12:38:43 -0500

    chart a path

--- spec/services/pathfinding/grid_spec.rb
+++ spec/services/pathfinding/grid_spec.rb
@@ -15,6 +15,15 @@ RSpec.describe Pathfinding::Grid, type: :service do
       path = finder.find_path(start_node, end_node, grid)
 
       ap path
+
+      charted_path = Pathfinding::Grid.chart_path(path)
+
+      charted_path.each do |direction|
+        Retroarch::KeyboardService.left if direction == 'left'
+        Retroarch::KeyboardService.right if direction == 'right'
+        Retroarch::KeyboardService.up if direction == 'up'
+        Retroarch::KeyboardService.down if direction == 'down'
+      end
     end
   end
 end
