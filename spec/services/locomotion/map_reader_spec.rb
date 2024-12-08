56d9077bdc41739fa7d4781d0d210d3a36c31fca
Andrew Denta <andrew.denta@gmail.com>
2024-12-07 21:29:09 -0500

    add initial specs

--- spec/services/locomotion/map_reader_spec.rb
+++ spec/services/locomotion/map_reader_spec.rb
@@ -1,29 +1,31 @@
 require 'rails_helper'
 
 RSpec.describe Locomotion::MapReader, type: :service do
+  @pid = nil
   before(:all) do
     command = '/Applications/RetroArch.app/Contents/MacOS/RetroArch -L "/Users/andrew/Library/Application Support/RetroArch/cores/mgba_libretro.dylib" "./db/data/games/firered.gba" --appendconfig="/Users/andrew/fire_red_agent/db/data/retroarch_config/savestate.cfg"'
-    system(command)
+    @pid = spawn(command)
+    Process.detach(@pid)
   end
 
   after(:all) do
-    raise NotImplementedError
+    Process.kill('KILL', @pid)
   end
   describe '.fetch_map_header' do
     it 'returns the correct map header data' do
-      # Add your test implementation here
+      sleep 4.seconds
     end
   end
 
   describe '.fetch_tile_data' do
     it 'returns the correct tile data' do
-      # Add your test implementation here
+      sleep 4.seconds
     end
   end
 
   describe '.fetch_map_dimensions' do
     it 'returns the correct map dimensions' do
-      # Add your test implementation here
+      sleep 4.seconds
     end
   end
 end
